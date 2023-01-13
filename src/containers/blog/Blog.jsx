168d90715fe7e7dea20cb0648a14789ab673a6fd
davedev <davidabolade29@gmail.com>
2023-01-14 00:22:46 +0100

    update: blog container completed

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import  Article  from '../../components/article/Article'
 import "./blog.css"
+import {blog01, blog02, blog03, blog04, blog05} from "./import"
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='gpt3__blog section__padding'>
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>A lot is happening, We are blogging about it.</h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article imgUrl={blog01} date="" text=""/>
+        </div>
+        <div className="gpt3__blog-container_groupB">
+          <Article imgUrl={blog02} date="" text=""/>
+          <Article imgUrl={blog03} date="" text=""/>
+          <Article imgUrl={blog04} date="" text=""/>
+          <Article imgUrl={blog05} date="" text=""/>
+        </div>
+      </div>
+    </div> 
   )
 }
 
